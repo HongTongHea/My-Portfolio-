e0309e8f69ef1ecf3b9370d6619c2c6b5d55973a
HongTongHea <tonghear1111@gmail.com>
2025-06-24 18:41:44 +0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Navigation } from "./components/Navigation";
 import { Hero } from "./components/Hero";
 import { About } from "./components/About";
 import { Projects } from "./components/Projects";
-// import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 
 function App() {
