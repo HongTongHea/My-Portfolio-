8bd8b3ee0189682974ed534587f437bd3baa8d6c
HongTongHea <165672527+HongTongHea@users.noreply.github.com>
2025-09-17 08:37:13 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"], 
+      },
       animation: {
         louder: "louder 0.5s ease-in-out",
       },
